3c27933338c315410f7948301a0d24e2f33f376b
nick.no.cave@gmail.com <nick.no.cave@gmail.com>
2016-06-04 12:00:00 +0300

    some commit change startapp

--- src/ua/artcode/taxi/run/StartApp.java
+++ src/ua/artcode/taxi/run/StartApp.java
@@ -1,8 +1,6 @@
 package ua.artcode.taxi.run;
 
-import ua.artcode.taxi.dao.AppDB;
-import ua.artcode.taxi.dao.OrderDaoInnerDbImpl;
-import ua.artcode.taxi.dao.UserDaoInnerDbImpl;
+import ua.artcode.taxi.dao.*;
 import ua.artcode.taxi.service.UserService;
 import ua.artcode.taxi.service.UserServiceImpl;
 import ua.artcode.taxi.service.ValidatorImpl;
@@ -12,9 +10,13 @@ public class StartApp {
 
 
     public static void main(String[] args) {
-        UserService userService = new UserServiceImpl(new UserDaoInnerDbImpl(new AppDB()),
-                                            new OrderDaoInnerDbImpl(new AppDB()),
-                                                    new ValidatorImpl(new AppDB()));
+
+        AppDB appDB = new AppDB();
+        UserDao userDao = new UserDaoInnerDbImpl(appDB);
+        OrderDao orderDao = new OrderDaoInnerDbImpl(appDB);
+        ValidatorImpl validator = new ValidatorImpl(appDB);
+
+        UserService userService = new UserServiceImpl(userDao, orderDao, validator);
         new UserLogin(userService);
 
     }
